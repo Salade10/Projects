5f1a51c95b597f3cf1bdb81e91f79c006daaf8f6
Noah Lockley <digitalprivacyisaright@gmail.com>
2026-03-10 09:53:25 +0000

    Add language support for English and French

---
+++ ComputerScience/ProwlerWordListGenCLI/Code/langs.py
@@ -0,0 +1,9 @@
+english = {"menu_text": "Entering prowler interactive shell. Type 'exit' to quit.",
+           "hdh":"hhdh"}
+
+francais = {"menu_text": "Entrée dans le shell interactif de Prowler. Tape 'exit' pour quitter. "}
+
+translations = {
+    "eng": english,
+    "fr": francais
+}
