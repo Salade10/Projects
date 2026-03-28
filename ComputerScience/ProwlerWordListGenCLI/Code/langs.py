d23b5f89ea83004b5d727f434f897a23c22b5742
Noah Lockley <digitalprivacyisaright@gmail.com>
2026-03-28 11:24:49 +0000

    Update langs.py

--- ComputerScience/ProwlerWordListGenCLI/Code/langs.py
+++ ComputerScience/ProwlerWordListGenCLI/Code/langs.py
@@ -1,7 +1,12 @@
 english = {"menu_text": "Entering prowler interactive shell. Type 'exit' to quit.",
-           "hdh":"hhdh"}
+           "subprocess_error_CalledProcessError":"It was impossible to open the file via cmd",
+           "FileNotFoundError":"The file could not be found"}
 
-francais = {"menu_text": "Entrée dans le shell interactif de Prowler. Tape 'exit' pour quitter. "}
+
+
+francais = {"menu_text": "Entrée dans le shell interactif de Prowler. Tape 'exit' pour quitter. ",
+            "subprocess_error_CalledProcesError":"C'etait impossible d'ouvrire le fichier avec le CMD.",
+            "FileNotFoundError":"Le fichier n'existe pas"}
 
 translations = {
     "eng": english,
